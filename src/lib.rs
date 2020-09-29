ab85b8896e99464d69ac600f588f539900c83d50
kieranricardo <kieranricardo@hotmail.com>
2020-09-29 10:54:01 +1000

    more docstrings + add benchmarks to test code

--- src/lib.rs
+++ src/lib.rs
@@ -60,6 +60,14 @@ fn two_sided_geometric(scale: f64) -> f64 {
 
 
 fn vectorize(scale: f64, num: usize, func: fn(f64) -> f64) -> Vec<f64> {
+    /// Vectorize a distribution sampler
+    ///
+    /// # Arguments
+    ///
+    /// * `scale` - The scale parameter of the distribution
+    /// * `num` - The number of samples to draw
+    /// * `func` - The distribution function
+
     let mut samples: Vec<f64> = vec![0.0; num];
     samples.par_iter_mut().for_each(|p| *p = func(scale));
     samples
--- tests/test_samplers.py
+++ tests/test_samplers.py
@@ -2,7 +2,7 @@ from differential_privacy.samplers import uniform, exponential, laplace, geometr
 import numpy as np
 
 
-def test_uniform():
+def test_uniform(benchmark):
     for num in [1, 20, 100, 1000]:
         samples = uniform(num)
         assert samples.shape == (num,)
@@ -10,9 +10,10 @@ def test_uniform():
     large_sample = uniform(10000000)
     assert np.isclose(large_sample.mean(), 0.5, rtol=0.01, atol=0.01)
     assert np.isclose(large_sample.var(), 1 / 12, rtol=0.01, atol=0.01)
+    benchmark(lambda: uniform(1000000, 1.0))
 
 
-def test_exponential():
+def test_exponential(benchmark):
     for num in [1, 20, 100, 1000]:
         samples = exponential(num, 1.0)
         assert samples.shape == (num,)
@@ -21,9 +22,10 @@ def test_exponential():
     large_sample = exponential(10000000, scale)
     assert np.isclose(large_sample.mean(), scale, rtol=0.01, atol=0.01)
     assert np.isclose(large_sample.var(), scale ** 2, rtol=0.01, atol=0.01)
+    benchmark(lambda: exponential(1000000, 1.0))
 
 
-def test_laplace():
+def test_laplace(benchmark):
     for num in [1, 20, 100, 1000]:
         samples = laplace(num, 1.0)
         assert samples.shape == (num,)
@@ -32,10 +34,11 @@ def test_laplace():
     large_sample = laplace(10000000, scale)
     assert np.isclose(large_sample.mean(), 0, rtol=0.01, atol=0.01)
     assert np.isclose(large_sample.var(), 2 * scale ** 2, rtol=0.01, atol=0.01)
+    benchmark(lambda: laplace(1000000, 1.0))
 
 
 # @pytest.mark.xfail(reason="possible bug in implementation")
-def test_geometric():
+def test_geometric(benchmark):
     for num in [1, 20, 100, 1000]:
         samples = geometric(num, 0.5)
         assert samples.shape == (num,)
@@ -44,9 +47,10 @@ def test_geometric():
     large_sample = geometric(10000000, scale)
     assert np.isclose(large_sample.mean(), (1 - scale) / scale, rtol=0.01, atol=0.01)
     assert np.isclose(large_sample.var(), (1 - scale) / scale ** 2, rtol=0.01, atol=0.01)
+    benchmark(lambda: geometric(1000000, 1.0))
 
 
-def test_two_sided_geometric():
+def test_two_sided_geometric(benchmark):
     for num in [1, 20, 100, 1000]:
         samples = two_sided_geometric(num, 0.5)
         assert samples.shape == (num,)
@@ -55,3 +59,5 @@ def test_two_sided_geometric():
     large_sample = two_sided_geometric(10000000, scale)
     assert np.isclose(large_sample.mean(), 0, rtol=0.01, atol=0.01)
     assert np.isclose(large_sample.var(), 2 * (scale) / (1 - scale) ** 2, rtol=0.01, atol=0.01)
+
+    benchmark(lambda: two_sided_geometric(1000000, 1.0))
