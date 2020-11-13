075e2ce320f9afa64c0fc2f3113c284e5aaa451c
Michael Purcell <Michael.Purcell1@anu.edu.au>
2020-11-13 14:21:48 +1100

    Added some basic usage instructions to README.md

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ First of all, install rust: https://doc.rust-lang.org/book/ch01-01-installation.
 Then:
 
 ```
-$ git clone 
+$ git clone
 $ cd differential-privacy
 $ pip install .
 ```
@@ -36,3 +36,17 @@ sphinx-build -b html docs-source docs-build
 ```
 
 The docs will now be in `docs-build/index.html`.
+
+## Basic Usage
+```python
+# Read the data and compute the exact query responses.
+import pandas as pd
+data = pd.read_csv("pcr_testing_age_group_2020-03-09.csv")
+raw_age_counts = data["age_group"].value_counts().sort_index()
+
+# Create a differentially private release mechanism.
+from differential_privacy.mechanisms import GeometricMechanism
+mechanism = GeometricMechanism(epsilon=0.1)
+# Apply the release mechanism to the exact query responses.
+dp_age_counts = mechanism.release(values=raw_age_counts.values)
+```
--- examples/differentially_private_histogram.py
+++ examples/differentially_private_histogram.py
@@ -1,22 +1,33 @@
 import numpy as np
 import pandas as pd
+
 import matplotlib.pyplot as plt
 
 from differential_privacy.mechanisms import GeometricMechanism
 
+# Read the data and compute the exact query responses.
 data = pd.read_csv("pcr_testing_age_group_2020-03-09.csv")
+raw_age_counts = data["age_group"].value_counts().sort_index()
 
-age_groups = np.sort(data['age_group'].unique())
-raw_age_counts = data['age_group'].value_counts().sort_index()
+# Create a differentially private release mechanism.
+mechanism = GeometricMechanism(epsilon=0.1)
+# Apply the release mechanism to the exact query responses.
+dp_age_counts = mechanism.release(values=raw_age_counts.values)
 
-epsilon = 0.1
-mechanism = GeometricMechanism(epsilon)
-dp_age_counts = mechanism.release(raw_age_counts.values)
+# Display the exact query responses alongside the output of the
+# differentially private release mechanism.
+age_groups = np.sort(data["age_group"].unique())
+age_ranges = np.array([a.lstrip("AgeGroup_") for a in age_groups])
+df = pd.DataFrame(
+    (
+        {
+            "Age Group": age_ranges,
+            "Exact Counts": raw_age_counts,
+            "Perturbed Counts": dp_age_counts,
+        }
+    )
+)
+print(df)
 
-age_ranges = np.array([a.lstrip('AgeGroup_') for a in age_groups])
-df = pd.DataFrame(({'Age Group'      : age_ranges,
-                    'Exact Count'    : raw_age_counts,
-                    'Perturbed Count': dp_age_counts}))
-
-df.plot(x='Age Group', title="Counts of Tests by Age Group", kind='bar', rot=0)
+df.plot(x="Age Group", title="Test Counts by Age Group", kind="bar", rot=0)
 plt.show()
