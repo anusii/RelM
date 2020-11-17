7c714f4b2d75ac65f43a36319da7d24f3d1b98dd
Michael Purcell <Michael.Purcell1@anu.edu.au>
2020-11-17 14:47:22 +1100

    Playing with hyperparameters again.

--- examples/histogram_query.py
+++ examples/histogram_query.py
@@ -4,7 +4,7 @@ import pathlib
 
 from relm.mechanisms import GeometricMechanism
 
-EPSILON = 2 ** -4
+EPSILON = 2 ** -3
 SENSITIVITY = 1.0
 
 # ========================================================================================
