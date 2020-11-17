99bd34c12f7b774a6fccc53a09023727483732c4
Michael Purcell <Michael.Purcell1@anu.edu.au>
2020-11-17 11:46:03 +1100

    black formatting

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,9 +2,10 @@ import pathlib
 import runpy
 import pytest
 
-path = pathlib.Path(__file__, '..', '..', 'examples').resolve()
-scripts = path.glob('*.py')
+path = pathlib.Path(__file__, "..", "..", "examples").resolve()
+scripts = path.glob("*.py")
 
-@pytest.mark.parametrize('script', scripts)
+
+@pytest.mark.parametrize("script", scripts)
 def test_script_execution(script):
     runpy.run_path(script)
