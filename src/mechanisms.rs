16192601b5dc45715626f65f4006012e5926c709
Michael Purcell <mike@Michaels-MBP.lan>
2021-03-05 13:46:34 +1100

    More probing

--- src/mechanisms.rs
+++ src/mechanisms.rs
@@ -22,14 +22,14 @@ pub fn all_above_threshold(data: Vec<f64>, epsilon: f64, threshold: f64, precisi
 }
 
 
-pub fn snapping(data: Vec<f64>, bound: f64, lambda: f64, quanta: f64) -> Vec<f64> {
-    data.par_iter()
-        .map(|&p| utils::clamp(p, bound))
-        .map(|p| p + lambda * utils::ln_rn(samplers::uniform(1.0)) * (samplers::uniform(1.0) - 0.5).signum())
-        .map(|p| quanta * (p / quanta).round())
-        .map(|p| utils::clamp(p, bound))
-        .collect()
-}
+// pub fn snapping(data: Vec<f64>, bound: f64, lambda: f64, quanta: f64) -> Vec<f64> {
+//     data.par_iter()
+//         .map(|&p| utils::clamp(p, bound))
+//         .map(|p| p + lambda * utils::ln_rn(samplers::uniform(1.0)) * (samplers::uniform(1.0) - 0.5).signum())
+//         .map(|p| quanta * (p / quanta).round())
+//         .map(|p| utils::clamp(p, bound))
+//         .collect()
+// }
 
 
 pub fn laplace_mechanism(data: Vec<f64>, epsilon: f64, precision: i32) -> Vec<f64> {
--- tests/test_samplers.py
+++ tests/test_samplers.py
@@ -4,7 +4,7 @@ import scipy.stats
 import relm.backend
 
 
-def test_uniform_sampler():
-    samples = relm.backend.sample_uniform(1.0, 1_000_000)
-    score, pval = scipy.stats.kstest(samples, 'uniform')
-    assert pval > 0.001
+# def test_uniform_sampler():
+#     samples = relm.backend.sample_uniform(1.0, 1_000_000)
+#     score, pval = scipy.stats.kstest(samples, 'uniform')
+#     assert pval > 0.001
