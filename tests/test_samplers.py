d94aa96588dc19c1eed8985854c46a0ea8bd296b
Michael Purcell <mike@Michaels-MBP.lan>
2021-03-09 11:26:18 +1100

    black formatting

--- tests/test_samplers.py
+++ tests/test_samplers.py
@@ -6,5 +6,5 @@ import relm.backend
 
 def test_uniform_sampler():
     samples = relm.backend.sample_uniform(1.0, 1_000_000)
-    score, pval = scipy.stats.kstest(samples, 'uniform')
+    score, pval = scipy.stats.kstest(samples, "uniform")
     assert pval > 0.001
