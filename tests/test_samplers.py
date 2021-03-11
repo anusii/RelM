8b25a998465f44b3a725f138d02453819284d773
Jakub Nabaglo <j@nab.gl>
2021-03-12 10:16:14 +1100

    Extra tests

--- tests/test_samplers.py
+++ tests/test_samplers.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import scipy.stats
 
@@ -5,6 +7,25 @@ import relm.backend
 
 
 def test_uniform_sampler():
-    samples = relm.backend.sample_uniform(1.0, 1_000_000)
-    score, pval = scipy.stats.kstest(samples, "uniform")
-    assert pval > 0.001
+    SCALES = [1.0, -1.0, 2.0, -.5, -math.tau, 1/math.e,
+              1.2250738585072009e-308]
+    for scale in SCALES:
+        samples = relm.backend.sample_uniform(scale, 1_000_000)
+
+        # Make sure samples have the correct sign/
+        assert (np.copysign(1., samples) == math.copysign(1., scale)).all()
+
+        # Take abs of scale and samples and verify it against
+        # scipy.stats.uniform.
+        scale = abs(scale)
+        samples = np.abs(samples)
+        score, pval = scipy.stats.kstest(samples, "uniform", args=(0, scale))
+        assert pval > 0.001
+
+
+def test_uniform_sampler_special_cases():
+    for scale in [0., -0., float('inf'), -float('inf'), float('NaN')]:
+        sample = relm.backend.sample_uniform(scale, 1)[0]
+        assert sample == scale or (math.isnan(scale) and np.isnan(sample))
+        # Preserves sign of negative zero:
+        assert math.copysign(1., sample) == np.copysign(1., scale)
